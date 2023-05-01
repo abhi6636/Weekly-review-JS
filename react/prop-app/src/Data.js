bd8a7681432693753c0332a11ef4cfb38322c33b
Abhinand <abhinandm94@gmail.com>
2023-05-01 23:06:38 +0530

    01/05/2023 commit

---
+++ react/prop-app/src/Data.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+        imgsrc:"https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib:rb-4.0.3&ixid:MnwxMjA3fDB8MHxzZWFyY2h8NHx8d2ViJTIwZGVzaWdufGVufDB8fDB8fA%3D%3D&auto:format&fit:crop&w:500&q:60",
+        title:"Card 1",
+        text:"This is card one",
+        btn:"Read More",
+    },
+    {
+        imgsrc : "https://images.unsplash.com/photo-1587440871875-191322ee64b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8d2ViJTIwZGVzaWdufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+          title:"Card 2",
+          text : "this is the card two",
+          btn : "Read Less",
+    },
+    {
+        imgsrc : "https://plus.unsplash.com/premium_photo-1661713438592-42a763d67cd6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8d2ViJTIwZGVzaWdufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+             title:"Card 2",
+             text : "this is the card three",
+             btn : "Read Nothing",
+    },
+    {
+        imgsrc : "https://images.unsplash.com/photo-1522542550221-31fd19575a2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHdlYiUyMGRlc2lnbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+        title:"Card 2",
+        text : "this is the card four",
+        btn : "Because its Dummy",
+    }
+]
+
+export {data}
