21ee75f33d854553a072d1d5045a0ac5a7fa91f9
Abhinand <abhinandm94@gmail.com>
2023-05-10 09:39:09 +0530

    10/05/2023 commit

---
+++ react/product-store/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header/Header";
+import Login from "./components/Login/Login";
+
+const App = () => {
+  return(
+    <>
+    <Header/>
+    <Login/>
+    </>
+  );
+};
+
+export default App;
